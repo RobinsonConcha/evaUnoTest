9637b6f1017930780fc1fa3ad04cfc1b1d17510a
RobinsonConcha <_patricio@hotmail.cl>
2023-10-09 20:39:05 -0300

    evaunotest commit

---
+++ src/main/java/com/iplacex/evaUnoTest/App.java
@@ -0,0 +1,22 @@
+package com.iplacex.evaUnoTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println( "****************************************" );
+        System.out.println( "Evaluación Uno Automatización de pruebas" );
+        System.out.println( "****************************************" );
+        System.out.println( "IPlacex Tecnológico Nacional************" );
+        System.out.println( "****************************************" );
+        System.out.println( "Evaluación Uno Automatización de pruebas" );
+        System.out.println( "****************************************" );
+        System.out.println( "****************************************" );
+        System.out.println( "************Hello World!****************" );
+        System.out.println( "****************************************" );
+    }
+}
